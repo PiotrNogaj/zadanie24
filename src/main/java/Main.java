0c60c0646545cb5b74c538e8151dfb8bc41608be
PiotrNogaj <161605894+PiotrNogaj@users.noreply.github.com>
2024-03-13 13:49:37 +0000

    Zadanie22

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,29 @@
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // import org.junit.jupiter.api.Test;
-
+import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
-    String nazwa = "Hello World!";
-    for(int i = 0; i < nazwa.length(); i++)
+    System.out.println("Podaj liczbę a:");
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    System.out.println("Podaj liczbę b:");
+    int b = scanner.nextInt();
+    System.out.println("Podaj liczbę c:");
+    int c = scanner.nextInt();
+
+    if(a > b && a > c)
     {
-    System.out.println(nazwa.charAt(i));
+      System.out.println("Największa liczba to a: " + a);
+    }
+    else if(b>a && b>c)
+    {
+      System.out.println("Największa liczba to b: " + b);
+    }
+    else if(c>a && c>b)
+    {
+      System.out.println("Największa liczba to c: " + c);
+      
     }
   }
  
