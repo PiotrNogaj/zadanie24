21cafbbf00de991a989d41bb4f7d179f28d7eb6b
PiotrNogaj <161605894+PiotrNogaj@users.noreply.github.com>
2024-03-13 13:32:30 +0000

    Zadanie2_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,11 @@
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!!!");
+    String nazwa = "Hello World!";
+    for(int i = 0; i < nazwa.length(); i++)
+    {
+    System.out.println(nazwa.charAt(i));
+    }
   }
 
   // @Test
