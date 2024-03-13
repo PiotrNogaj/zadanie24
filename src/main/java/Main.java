b19c1f2ff0976cbae026899ac67959f6dc05b185
PiotrNogaj <161605894+PiotrNogaj@users.noreply.github.com>
2024-03-13 14:32:49 +0000

    zadanie24

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,24 +5,52 @@ import java.util.Scanner;
 import java.util.Random;
 public class Main 
 {
-  public static int generateRandomNumber(int min, int max)
-  {
-    Random random = new Random();
-    return random.nextInt(max - min + 1) + min;
-  }
   public static void main(String[] args)
   {
-    int randomNumber1 = generateRandomNumber(0,100);
-    int randomNumber2 = generateRandomNumber(0,100);
-    int randomNumber3 = generateRandomNumber(0,100);
-    int randomNumber4 = generateRandomNumber(0,100);
-    int randomNumber5 = generateRandomNumber(0,100);
-    
+    Scanner scanner = new Scanner(System.in);
+    int choice = 2;
     int suma = 0;
+    int number = 0;
 
+    while (choice !=4)
+      {
+        System.out.println("Podaj co chcesz zrobić\n1. Dodawanie\n2. Odejmowanie\n3. Mnożenie\n4. Dzielenie\n5.Wyjście");
+        choice = scanner.nextInt();
     
-    suma = randomNumber1^2 + randomNumber2^ + randomNumber3^2 + randomNumber4^2+ randomNumber5^2;
-    System.out.println(suma);
+    switch (choice)
+      {
+        case 1:
+          number = scanner.nextInt();
+          suma = suma + number;
+          System.out.println("suma: " + suma);
+          break;
+
+        case 2:
+          number = scanner.nextInt();
+          suma = suma - number;
+          System.out.println("suma: " + suma);
+          break;
+
+        case 3:
+          number = scanner.nextInt();
+          suma = suma * number;
+          System.out.println("suma: " + suma);
+          break;
+
+        case 4: 
+          number = scanner.nextInt();
+          suma = suma / number;
+          System.out.println("suma: " + suma);
+          break;
+
+        case 5:
+          System.out.println("Wyjście");
+          break;
+          
+          
+          
+      }
+      }
   }
   
 }
